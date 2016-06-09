3336243984b9665ec05f079a822ac5b3aaec3b3f
Stephen Emery <emery.stephen@gmail.com>
2016-06-09 10:01:23 -0700

    Update checkbox variable inputs in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     var name = $("#nameInput").val();
     var age = parseInt($("#ageInput").val());
-    var landscape = $("input:checkbox[id=landscape]:checked");
-    var sounds = $("input:checkbox[id=sounds]:checked");
-    var nightlife = $("input:checkbox[id=nightlife]:checked");
+    var landscape = $("input:checkbox[name=landscape]:checked");
+    var sounds = $("input:checkbox[name=sounds]:checked");
+    var nightlife = $("input:checkbox[name=nightlife]:checked");
     var locationScore = 0;
 
     event.preventDefault();
