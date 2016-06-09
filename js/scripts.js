aa83edcba4393a3ff716c2e8a0c7aef0a5d6ddfb
Stephen Emery <emery.stephen@gmail.com>
2016-06-09 14:19:51 -0700

    Rename values on forms to make scripts.js logic easier.

--- js/scripts.js
+++ js/scripts.js
@@ -11,29 +11,32 @@ $(document).ready(function() {
     var locationScore = 0;
 
     // Unique results
+    if (landscape != sounds && landscape != nightlife && nightlife != sounds) {
+      $("#result-indecisive").show();
+    }
 
     // Results caulculations, beach = +6, city = +3, mountains = +1
-    if (landscape === "landscapeBeach") {
+    if (landscape === "beach") {
       locationScore += 6;
-    } else if (landscape === "landscapeCity") {
+    } else if (landscape === "city") {
       locationScore += 3;
-    } else if (landscape === "landscapeMountains") {
+    } else if (landscape === "mountains") {
       locationScore += 1;
     }
 
-    if (sounds === "soundsBeach") {
+    if (sounds === "beach") {
       locationScore += 6;
-    } else if (sounds === "soundsCity") {
+    } else if (sounds === "city") {
       locationScore += 3;
-    } else if (sounds === "soundsMountains") {
+    } else if (sounds === "mountains") {
       locationScore += 1;
     }
 
-    if (nightlife === "nightlifeBeach") {
+    if (nightlife === "beach") {
       locationScore += 6;
-    } else if (nightlife === "nightlifeCity") {
+    } else if (nightlife === "city") {
       locationScore += 3;
-    } else if (nightlife === "nightlifeMountains") {
+    } else if (nightlife === "mountains") {
       locationScore += 1;
     }
 
