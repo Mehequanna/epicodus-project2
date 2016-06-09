f0b60ad41bf5b90d1f21cfc92dbecbb58f7c281b
Stephen Emery <emery.stephen@gmail.com>
2016-06-09 15:09:34 -0700

    Add space.jpg to index, fix scoring issue in scripts.js
    With the points system there is a chance of overlapping in score. So I
    created an if statement that caused a -= 1 if certain conditions were
    met. This prevents scores overlapping.

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,12 @@ $(document).ready(function() {
 
     // Unique results
     if (landscape != sounds && landscape != nightlife && nightlife != sounds) {
-      $("#result-indecisive").show();
+      $("#result-indecisive").show("slow");
     }
 
     // Results caulculations, beach = +6, city = +3, mountains = +1
     if (landscape === "beach") {
-      locationScore += 6;
+      locationScore += 5;
     } else if (landscape === "city") {
       locationScore += 3;
     } else if (landscape === "mountains") {
@@ -25,7 +25,7 @@ $(document).ready(function() {
     }
 
     if (sounds === "beach") {
-      locationScore += 6;
+      locationScore += 5;
     } else if (sounds === "city") {
       locationScore += 3;
     } else if (sounds === "mountains") {
@@ -33,14 +33,27 @@ $(document).ready(function() {
     }
 
     if (nightlife === "beach") {
-      locationScore += 6;
+      locationScore += 5;
     } else if (nightlife === "city") {
       locationScore += 3;
     } else if (nightlife === "mountains") {
       locationScore += 1;
     }
 
+
+    if (locationScore === 7 && nightlife === "beach" || locationScore === 7 && sounds === "beach" || locationScore === 7 && landscape === "beach" || locationScore === 11 && nightlife === "beach" || locationScore === 11 && sounds === "beach" || locationScore === 11 && landscape === "beach") {
+      locationScore -= 1;
+    }
     console.log(locationScore);
+
+    // Actions based on locationScore
+    if (locationScore <= 6 ) {
+      $("#result-mountains").show('slow');
+    } else if (locationScore <= 10) {
+      $("#result-city").show('slow');
+    } else if (locationScore <= 15) {
+      $("#result-beach").show('slow');
+    }
 
     // $("#result-beach, #result-city").show();
 
